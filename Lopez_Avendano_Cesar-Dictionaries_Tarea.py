43d001ce69139aec7838dac9281270b8e850d1f8
CesarL07 <166559917+CesarL07@users.noreply.github.com>
2024-04-09 15:42:34 -0700

    Add files via upload

---
+++ Lopez_Avendano_Cesar-Dictionaries_Tarea.py
@@ -0,0 +1,16 @@
+#Dictionaries_Tarea1
+students_in_math={
+    "Jael":8,
+    "Jose":7,   
+    "Derek":9,
+    "René":10,
+    "Manuel":9,
+    "Guillermo":8,
+    "Angel":10,
+    "Santiago":9,
+    "Said":7,
+    "Ana":10,
+    "Carolina":9    
+}
+for key,value in students_in_math.items():
+    print("El/La alumno(a):",key,",tiene una calificacion de:",value)
