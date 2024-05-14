e015d69fb2b66b58c50b370115b2d8f83c5fbb6e
CesarL07 <166559917+CesarL07@users.noreply.github.com>
2024-05-14 16:55:02 -0700

    #Tarea Graficar Parabola.py

--- #Tarea Graficar Parabola.py
+++ #Tarea Graficar Parabola.py
@@ -1,7 +1,30 @@
-#Tarea Graficar Parabola
-#Para este codigo se asume que en el tiro parabolico, el tiempo de subida es el mismo que el de bajada
+#Tarea Graficar Parábola
+#Para este código se asume que en el tiro parabólico, el tiempo de subida es el mismo que el de bajada
 import math
 import turtle as t
+
+def dibujar_ejes():
+    #Con este ciclo dibujamos al eje "y" y sus correspondientes valores
+    t.penup()
+    t.goto(0, -350)
+    t.pendown()
+    t.goto(0, 350)
+    for i in range(-350, 350, 40):
+        t.penup()
+        t.goto(-20, i)
+        t.pendown()
+        t.write(str(i))
+
+    #Con este otro dibujamos al eje "x" y sus correspondientes valores
+    t.penup()
+    t.goto(-620, 0)
+    t.pendown()
+    t.goto(620, 0)
+    for i in range(-620, 620, 40):
+        t.penup()
+        t.goto(i, -15)
+        t.pendown()
+        t.write(str(i))
 
 def parabola(pos_ini,vi,ang):
     t.pu()
@@ -12,21 +35,20 @@ def parabola(pos_ini,vi,ang):
     v1x=vi*math.cos((ang*math.pi)/180)
     v1y=vi*math.sin((ang*math.pi)/180)
     t_vuelo=(2*v1y)/g
-    puntos_totales=10
+    puntos_totales=100
     t_por_punto=t_vuelo/puntos_totales
 
     t.setpos(xi,yi)
     t.pd()
 
-    for i in range(11):
+    for i in range(101):
         xf=xi+v1x*i*t_por_punto
         yf=yi+(v1y*i*t_por_punto)-((1/2)*g*((i*t_por_punto)**2))
         t.setpos(xf,yf)
-        print(xf,yf)
     t.mainloop()
 
 pi=(-620,0)
 vi=110
 ang=45
-
+dibujar_ejes()
 parabola(pi,vi,ang)
